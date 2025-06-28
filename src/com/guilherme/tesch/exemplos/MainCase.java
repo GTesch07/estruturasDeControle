381d3bca2fc3ab0b0e6f457af5da3926fbb19596
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-28 13:48:20 -0300

    Trabalhando com switch case.

---
+++ src/com/guilherme/tesch/exemplos/MainCase.java
@@ -0,0 +1,47 @@
+package com.guilherme.tesch.exemplos;
+
+import java.util.Scanner;
+
+public class MainCase {
+    public static void main(String[]args){
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe um número de 1 até 7");
+        var option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+
+                /*OU
+                 case 1 -> System.out.println("Domingo");
+                 case 2 -> System.out.println("Segunda");
+                 case 3 -> System.out.println("Terça");
+                 case 5 -> System.out.println("Quarta");
+                 default -> System.out.println("Opção inválida")
+                 */
+        }
+
+        scanner.close();
+    }
+}
