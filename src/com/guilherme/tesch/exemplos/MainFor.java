07f47cc63c3ce4469cecc66d8170d24db8e81022
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-28 14:21:05 -0300

    Trabalhando com for.

---
+++ src/com/guilherme/tesch/exemplos/MainFor.java
@@ -0,0 +1,31 @@
+package com.guilherme.tesch.exemplos;
+
+import java.util.Scanner;
+
+public class MainFor {
+    public static void main(String[]args){
+        var scanner = new Scanner(System.in);
+      
+        for(var i = 200;i >= 0 ;i -=2 ){
+           
+            
+            System.out.println(i);
+        }
+       
+       
+       
+       
+       
+       
+       
+        /*for(;;){
+            System.out.println("Digite um nome: ");
+            var name = scanner.next();
+
+            if (name.equalsIgnoreCase("exit"))break;
+            System.out.println(name);
+        }
+        */
+      
+    }
+}
