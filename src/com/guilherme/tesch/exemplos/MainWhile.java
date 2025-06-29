ffd3b1e6eca51e723db2ac6c5a915e6fa4d2d177
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-29 20:27:52 -0300

    Utilizando do while.

--- src/com/guilherme/tesch/exemplos/MainWhile.java
+++ src/com/guilherme/tesch/exemplos/MainWhile.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class MainWhile {
     public static void main(String[] args){
         var scanner = new Scanner(System.in);
-        var name = "";
-        while (true) {
-            System.out.println("Informe um nome: ");
-            name = scanner.next();
-            System.out.println(name);
-
-            if (name.equalsIgnoreCase("exit")) break;
-                
-            
+        var i = 0;
+        while (args.length > i) {
+            System.out.println(args[i]);
+            i++;
         }
+
+        i = 0;
+        do{
+            System.out.println(args[i]);
+            i ++;
+                //Código é rodado pelo menos uma vez antes de entrar na estrutura While
+        }while(args.length > i);
+       
     }
 }
