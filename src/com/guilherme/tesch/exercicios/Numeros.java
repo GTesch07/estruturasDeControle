4996289cb3b2f4208092b1013bafc1d298c921dc
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-29 22:08:15 -0300

    Exercício 4: não entendi o sentido do código só fiz :)

---
+++ src/com/guilherme/tesch/exercicios/Numeros.java
@@ -0,0 +1,25 @@
+package com.guilherme.tesch.exercicios;
+
+import java.util.Scanner;
+
+public class Numeros {
+    public static void main(String[] args){
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        var number = scanner.nextInt();
+        var keepVerify = true;
+        while (keepVerify) {
+            System.out.println("Informe o número para verificação: ");
+            var toVerify = scanner.nextInt();
+            if (toVerify < number) {
+                System.out.printf("Informe um número maior que %s\n",number);
+                continue;
+            }
+            var result = toVerify % number;
+            keepVerify = result == 0;
+            System.out.printf("%s %% %s = %s\n",toVerify, number, result);
+        }
+        scanner.close();
+       
+    }
+}
